23a54de8ed72235c1bd3e1e3cc030b446a210376
tinshu48 <tinshu48@gmail.com>
2024-05-29 07:36:25 +0530

    changed map color

--- src/Components/indiaMap.jsx
+++ src/Components/indiaMap.jsx
@@ -2,7 +2,40 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-use-history';
 import IndiaMap from '@svg-maps/india';
 import { SVGMap } from 'react-svg-map';
-import 'react-svg-map/lib/index.css';
+import "./indiaMap.css";
+
+const stateColors = {
+    AndhraPradesh: "#FF6347",
+    ArunachalPradesh: "#4682B4",
+    Assam: "#32CD32",
+    Bihar: "#FFD700",
+    Chhattisgarh: "#FF69B4",
+    Delhi: "#8A2BE2",
+    Goa: "#8A2BE2",
+    Gujarat: "#FF4500",
+    Haryana: "#2E8B57",
+    HimachalPradesh: "#DA70D6",
+    Jharkhand: "#1E90FF",
+    Karnataka: "#FF1493",
+    Kerala: "#00FF7F",
+    MadhyaPradesh: "#8B0000",
+    Maharashtra: "#808000",
+    Manipur: "#00CED1",
+    Meghalaya: "#FF00FF",
+    Mizoram: "#8A2BE2",
+    Nagaland: "#FF8C00",
+    Odisha: "#B8860B",
+    Punjab: "#32CD32",
+    Rajasthan: "#8B4513",
+    Sikkim: "#A52A2A",
+    TamilNadu: "#7FFF00",
+    Telangana: "#D2691E",
+    Tripura: "#6495ED",
+    UttarPradesh: "#DC143C",
+    Uttarakhand: "#008B8B",
+    WestBengal: "#B22222",
+    JammuKashmir: "#6A5ACD" // Adding color for Jammu and Kashmir
+};
 
 const Map = () => {
     const [selectedState, setSelectedState] = useState(null);
@@ -10,10 +43,10 @@ const Map = () => {
     const history = useHistory();
 
     const handleLocationClick = (event) => {
-        const stateName = event.target.attributes.name?.value;
+        const stateName = event.target.attributes.name?.value.replace(/\s+/g, '');
         if (stateName) {
             setSelectedState(stateName);
-            const formattedStateName = stateName.toLowerCase().replace(/\s+/g, '');
+            const formattedStateName = stateName.toLowerCase();
             history.push(`/state/${encodeURIComponent(formattedStateName)}`);
         } else {
             console.error('State name is undefined or not found');
@@ -39,12 +72,9 @@ const Map = () => {
                 map={IndiaMap}
                 className="w-full h-auto stroke-black stroke-1"
                 onLocationClick={handleLocationClick}
-                customize={{
-                    Bihar: {
-                        fill: "red",
-                        stroke: "#000",
-                        strokeWidth: 1
-                    }
+                locationClassName={(location) => {
+                    const stateName = location.name.replace(/\s+/g, '');
+                    return `svg-map__location ${stateName}`;
                 }}
             />
         </div>

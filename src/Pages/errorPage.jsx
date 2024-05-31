f5b282e24c47e671901ac56413dfde82ae5a39a0
tinshu48 <tinshu48@gmail.com>
2024-05-31 14:43:17 +0530

    error page responsive

--- src/Pages/errorPage.jsx
+++ src/Pages/errorPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import errorImage from '../asset/error.jpg'; // Make sure you have an error image in the specified path
+import errorImage from '../asset/error.jpg';
 
 const ErrorPage = () => {
     const navigate = useNavigate();
@@ -10,10 +10,10 @@ const ErrorPage = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <img src={errorImage} alt="Error" className="w-auto h-auto" />
-            <h1 className="text-4xl font-bold mt-8">Oops! Page Not Found</h1>
-            <p className="text-lg mt-4">Sorry, the page you are looking for does not exist.</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white-100 p-4">
+            <img src={errorImage} alt="Error" className="w-full max-w-md h-auto" />
+            <h1 className="text-2xl md:text-4xl font-bold mt-8 text-center">Oops! Page Not Found</h1>
+            <p className="text-md md:text-lg mt-4 text-center">Sorry, the page you are looking for does not exist.</p>
             <button
                 onClick={handleGoBack}
                 className="mt-8 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"

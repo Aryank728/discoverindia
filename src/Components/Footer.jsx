ed1775bd85391eb92646fab7ad2b93737c086c80
tinshu48 <tinshu48@gmail.com>
2024-06-04 09:32:51 +0530

    redesigned

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaGithub, FaLinkedin, FaGlobe } from 'react-icons/fa';
 
 const Footer = () => {
     const quotes = [
@@ -24,14 +25,19 @@ const Footer = () => {
 
     return (
         <footer className='bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 py-8 px-3'>
-            <div className='container mx-auto flex flex-col lg:flex-row justify-between items-center'>
-                <div className='flex flex-col lg:flex-row items-center lg:items-start text-center lg:text-left'>
+            <div className='container mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center'>
+                <div className='flex flex-col lg:items-start text-center lg:text-left lg:w-1/2'>
                     <p className='text-white font-poppins font-bold'>
                         Discover India <br />
                         <span>All rights reserved &copy; 2024</span>
                     </p>
+                    <div className='flex flex-row gap-x-8 list-none mt-4'>
+                        <a href='https://github.com/Aryank728' className='text-white text-2xl'><FaGithub /></a>
+                        <a href='https://www.linkedin.com/in/Aryank728' className='text-white text-2xl'><FaLinkedin /></a>
+                        <a href='https://aryankr.netlify.app' className='text-white text-2xl'><FaGlobe /></a>
+                    </div>
                 </div>
-                <div className='w-full lg:w-1/3 mt-8 lg:mt-0 mx-auto lg:mx-0'>
+                <div className='w-full lg:w-1/2 mt-8 lg:mt-0 mx-auto lg:mx-0'>
                     <Slider {...settings}>
                         {quotes.map((quote, index) => (
                             <div key={index} className="quote-slide p-4">
@@ -42,11 +48,6 @@ const Footer = () => {
                             </div>
                         ))}
                     </Slider>
-                </div>
-                <div className='flex flex-col lg:flex-row gap-x-8 gap-y-4 list-none mt-4 lg:mt-0 lg:ml-8'>
-                    <a href='https://github.com/Aryank728' className='text-white'>Github</a>
-                    <a href='https://www.linkedin.com/in/Aryank728' className='text-white'>LinkedIn</a>
-                    <a href='https://aryankr.netlify.app' className='text-white'>Portfolio</a>
                 </div>
             </div>
         </footer>
